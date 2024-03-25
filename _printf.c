7b14b1ae66efbcd7fb8aad9a89a37035b2a17276
xhuve <xhuvelialbana@gmail.com>
2024-03-25 15:12:17 +0100

    Fixing

--- _printf.c
+++ _printf.c
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 	va_start(list, format);
 	while (format[i] != '\0')
 	{
-		if (format[i] == "%")
+		if (format[i] == '%')
 		{
 			j = 0;
 			while (prints[j].specifier != '\0')
